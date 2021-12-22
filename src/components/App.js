fa000557ce93aca8702e4e8515d65b886277a42b
Graphic Design <atosa@Graphics-iMac.local>
2021-12-21 16:07:51 -0800

    added styling to buttons, added UUIDv4, add and delete functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
 import React, { useState } from "react"
 import RecipeList from "./RecipeList"
 import "../css/app.css"
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
-  return <RecipeList recipes={sampleRecipes} />
+  const [recipes, setRecipes] = useState(sampleRecipes)
+  
+  const handleRecipeAdd = () => {
+    console.log(recipes)
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        {
+          id: uuidv4(), name: 'Name', amount: '1 Tbsp'
+        }
+      ]
+    }
+    setRecipes([...recipes, newRecipe])
+  }
+  const handleRecipeDelete = (id) => {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
+  return (  
+  <RecipeList 
+    recipes={recipes}
+    handleRecipeAdd={handleRecipeAdd} 
+    handleRecipeDelete={handleRecipeDelete}
+    />
+  )
 }
+
+
 
 const sampleRecipes = [
   {
@@ -12,7 +43,7 @@ const sampleRecipes = [
     name: "plain chicken",
     servings: 3,
     cookTime: "1:45",
-    instructions: "1. Salt on Chicken\n 2. Put chicken inside oven\n 3. Eat!",
+    instructions: "1. Salt on Chicken\n2. Put chicken inside oven\n3. Eat!",
     ingredients: [
       {
         id: 1,
@@ -31,7 +62,7 @@ const sampleRecipes = [
     name: "plain pork",
     servings: 5,
     cookTime: "0:45",
-    instructions: "1. Paprika on Pork\n 2. Put Pork inside oven\n 3. Eat!",
+    instructions: "1. Paprika on Pork\n2. Put Pork inside oven\n3. Eat!",
     ingredients: [
       {
         id: 1,
@@ -50,7 +81,7 @@ const sampleRecipes = [
     name: "Birria Tacos",
     servings: 12,
     cookTime: "4:50",
-    instructions: "1. Deseed Guajillo Chiles\n 2. Put chile in Pot\n 3. Eat!",
+    instructions: "1. Deseed Guajillo Chiles\n2. Put chile in Pot\n3. Eat!",
     ingredients: [
       {
         id: 1,
