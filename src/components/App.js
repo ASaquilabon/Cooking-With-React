994340a608eed5e1ac269771f9c804bf93fde6fa
archie saquilabon <asaquilabon@gmail.com>
2021-12-22 21:42:05 -0800

    added input form, text area, and useEffect Hook instead of prop drilling

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,57 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import RecipeList from "./RecipeList"
+import RecipeEdit from "./RecipeEdit"
 import "../css/app.css"
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid"
 
+export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = "cookingWithReact.recipes"
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes)
-  
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
   const handleRecipeAdd = () => {
-    console.log(recipes)
     const newRecipe = {
       id: uuidv4(),
-      name: 'New',
+      name: "New",
       servings: 1,
-      cookTime: '1:00',
-      instructions: 'Instr.',
+      cookTime: "1:00",
+      instructions: "Instr.",
       ingredients: [
         {
-          id: uuidv4(), name: 'Name', amount: '1 Tbsp'
-        }
-      ]
+          id: uuidv4(),
+          name: "Name",
+          amount: "1 Tbsp",
+        },
+      ],
     }
     setRecipes([...recipes, newRecipe])
   }
+
   const handleRecipeDelete = (id) => {
-    setRecipes(recipes.filter(recipe => recipe.id !== id))
+    setRecipes(recipes.filter((recipe) => recipe.id !== id))
   }
 
-  return (  
-  <RecipeList 
-    recipes={recipes}
-    handleRecipeAdd={handleRecipeAdd} 
-    handleRecipeDelete={handleRecipeDelete}
-    />
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete,
+  }
+
+  return (
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+      <RecipeEdit />
+    </RecipeContext.Provider>
   )
 }
-
-
 
 const sampleRecipes = [
   {
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,24 +1,19 @@
-import React from "react"
+import React, { useContext } from "react"
 import Recipe from "./Recipe"
+import { RecipeContext } from "./App"
 
-export default function RecipeList({ recipes, handleRecipeAdd, handleRecipeDelete }) {
+export default function RecipeList({ recipes }) {
+  const { handleRecipeAdd } = useContext(RecipeContext)
+
   return (
     <div className="recipe-list">
       <div>
         {recipes.map((recipe) => {
-          return (
-          <Recipe 
-          key={recipe.id}
-          handleRecipeDelete={handleRecipeDelete} 
-          {...recipe} />
-          )
+          return <Recipe key={recipe.id} {...recipe} />
         })}
       </div>
       <div className="recipe-list__add-recipe-btn-container">
-        <button 
-        className="btn btn--primary"
-        onClick={handleRecipeAdd}
-        >
+        <button className="btn btn--primary" onClick={handleRecipeAdd}>
           Add Recipe
         </button>
       </div>
